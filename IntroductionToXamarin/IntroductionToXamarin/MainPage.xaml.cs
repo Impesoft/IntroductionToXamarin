9fb68538de669d48f8aa37687ad5df67789c4725
CSharpCourse <CSharpCourse@yahoo.com>
2021-03-30 15:28:50 +0200

    Add project files.

---
+++ IntroductionToXamarin/IntroductionToXamarin/MainPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace IntroductionToXamarin
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnLoginButtonClicked(object sender, EventArgs e)
+        {
+            if(!string.IsNullOrEmpty(txtEmail.Text) && !string.IsNullOrEmpty(txtPassword.Text))
+            {
+                Navigation.PushAsync(new MenuPage());
+            }
+        }
+    }
+}
---
+++ IntroductionToXamarin/IntroductionToXamarin/Profile.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace IntroductionToXamarin
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Profile : ContentPage
+    {
+        public Profile()
+        {
+            InitializeComponent();
+        }
+
+        private void OnSaveInfo(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(firstName.Text) && !string.IsNullOrEmpty(lastName.Text))
+            {
+                ConcatInfo.Text = $"Welkom {firstName.Text} {lastName.Text}\nJe bent {age.Text}jaar jong en je hobby's zijn {Hobbies.Text}.";
+            }
+        }
+    }
+}
